8048d15cd3c19e8e52a481de29c43972d0a75fa7
Dao Tien Nam <dc.ctb.namdt@msb.com.vn>
2026-09-15 10:19:36 +0700

    update

--- app.py
+++ app.py
@@ -43,10 +43,10 @@ TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
 WATCH_SYMBOL_GROUPS = (
-    ("STB CTG TCB VPB SHB VCB HDB ACB", 40_000),  # bank
+    ("STB CTG TCB VPB SHB VCB HDB ACB BID", 40_000),  # bank
     ("SSI VIX HCM VND VCI TCX VPX VCK", 20_000),  # chứng
     ("NLG TCH KDH CII NVL DXG", 20_000),  # đất
-    ("GAS GVR BID", 20_000),  # Nhà nước
+    ("GAS GVR PLX", 20_000),  # Nhà nước
     ("BCM", 3000),
     ("FPT MSN DGW MCH BSR BVH GMD ANV", 20_000),  # Linh tinh
     ("VIC VHM GEX GEE VSC VGC", 20_000),  # Vin
