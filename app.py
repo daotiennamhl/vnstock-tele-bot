86b447d6e0b9805c59427f7b69cfd7d0a99666f4
Dao Tien Nam <dc.ctb.namdt@msb.com.vn>
2026-09-15 13:58:08 +0700

    update

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ WATCH_SYMBOL_GROUPS = (
     ("BCM", 3000),
     ("VIC VHM VRE GEX GEE VSC VGC", 20_000),  # Vin gex
     ("FPT MSN DGW MCH BSR BVH GMD ANV", 20_000),  # Linh tinh
-    ("PVD PVS OIL BSR"), # Dầu
+    ("PVD PVS OIL BSR", 20_000), # Dầu
 )
 
 WATCH_PORTFOLIO = {
